15786b20e03f901ad2a7cc3b7411dbb830937360
bilold1n <babdumutalov436@gmail.com>
2024-07-09 10:14:01 +0500

    vazifa

--- src/page/product/index.tsx
+++ src/page/product/index.tsx
@@ -1,7 +1,7 @@
 import usegetdata from "../hooks/usegetdata";
 export default function Product() {
   const { data, ispending, error } = usegetdata("products", "sa");
-  console.log(data);
+  console.log(data, ispending, error);
 
   return <div>Product</div>;
 }
