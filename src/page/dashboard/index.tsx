e41d04a28a4208b06e51af4fdb1b46943c519901
bilold1n <babdumutalov436@gmail.com>
2024-08-20 19:36:09 +0500

    yangi dashboard

--- src/page/dashboard/index.tsx
+++ src/page/dashboard/index.tsx
@@ -1,98 +1,206 @@
-import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
-import { Card, Col, Row, Statistic, Button } from "antd";
-import { LikeOutlined } from "@ant-design/icons";
-import { Input, QRCode, Space } from "antd";
 import React from "react";
+import ReactApexChart from "react-apexcharts";
 
-export default function Dashboard() {
-  const [text, setText] = React.useState("https://admin-shopco.netlify.app");
+const PieChart: React.FC = () => {
+  const pieOptions: any = {
+    chart: {
+      width: 380,
+      type: "pie",
+    },
+    labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            position: "bottom",
+          },
+        },
+      },
+    ],
+  };
+
+  const pieSeries = [44, 55, 13, 43, 22];
+
+  const barOptions: any = {
+    series: [
+      {
+        name: "Inflation",
+        data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 8.9],
+      },
+    ],
+    chart: {
+      height: 350,
+      type: "bar",
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 10,
+        dataLabels: {
+          position: "top",
+        },
+      },
+    },
+    dataLabels: {
+      enabled: true,
+      formatter: function (val: number) {
+        return val + "%";
+      },
+      offsetY: -20,
+      style: {
+        fontSize: "12px",
+        colors: ["#ffffff88"],
+      },
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      position: "top",
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+      crosshairs: {
+        fill: {
+          type: "gradient",
+          gradient: {
+            colorFrom: "#D8E3F0",
+            colorTo: "#BED1E6",
+            stops: [0, 100],
+            opacityFrom: 0.4,
+            opacityTo: 0.5,
+          },
+        },
+      },
+      tooltip: {
+        enabled: true,
+      },
+    },
+    yaxis: {
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+      labels: {
+        show: false,
+        formatter: function (val: number) {
+          return val + "%";
+        },
+      },
+    },
+    title: {
+      text: "One year statistics of shop.co online magazine",
+      floating: true,
+      offsetY: 330,
+      align: "center",
+      style: {
+        color: "#ffffff99",
+      },
+    },
+  };
+
+  const radialOptions: any = {
+    series: [17],
+    chart: {
+      height: 340,
+      type: "radialBar",
+      offsetY: -10,
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 135,
+        dataLabels: {
+          name: {
+            fontSize: "16px",
+            color: "#ffffff98",
+            offsetY: 120,
+          },
+          value: {
+            offsetY: 76,
+            fontSize: "22px",
+            color: "#ffffff",
+            formatter: function (val: number) {
+              return val + "%";
+            },
+          },
+        },
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        shadeIntensity: 0.15,
+        inverseColors: false,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 50, 65, 91],
+      },
+    },
+    stroke: {
+      dashArray: 4,
+    },
+    labels: ["Median Ratioy"],
+  };
+
   return (
-    <>
-      <div className="container flex flex-col gap-5 w-[90%] items-center">
-        <Row className="w-full" gutter={16}>
-          <Col span={12}>
-            <Card bordered={false}>
-              <Statistic
-                title="Active"
-                value={11.28}
-                precision={2}
-                valueStyle={{ color: "#3f8600" }}
-                prefix={<ArrowUpOutlined />}
-                suffix="%"
-              />
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card bordered={false}>
-              <Statistic
-                title="Idle"
-                value={9.3}
-                precision={2}
-                valueStyle={{ color: "#cf1322" }}
-                prefix={<ArrowDownOutlined />}
-                suffix="%"
-              />
-            </Card>
-          </Col>
-        </Row>
-        <div className="flex  justify-between w-full p-4">
-          <Row className="bg-[#fff] w-[50%] rounded-2xl p-2" gutter={16}>
-            <Col span={12}>
-              <Statistic
-                title="Active Users"
-                valueStyle={{ color: "#3f8600" }}
-                value={112893}
-              />
-            </Col>
-            <Col span={12}>
-              <Statistic
-                title="Account Balance (CNY)"
-                valueStyle={{ color: "#3f8600" }}
-                value={112893}
-                precision={2}
-              />
-              <Button style={{ marginTop: 16 }} type="primary">
-                Recharge
-              </Button>
-            </Col>
-            <Col span={12}>
-              <Statistic
-                title="Active Users"
-                valueStyle={{ color: "#3f8600" }}
-                value={112893}
-                loading
-              />
-            </Col>
-          </Row>
-          <Row
-            className="bg-[#fff] w-[50%] rounded-2xl flex items-center justify-center "
-            gutter={16}
-          >
-            <Col span={12}>
-              <Statistic
-                title="Feedback"
-                value={1128}
-                prefix={<LikeOutlined />}
-              />
-            </Col>
-            <Col span={12}>
-              <Statistic title="Unmerged" value={93} suffix="/ 100" />
-            </Col>
-          </Row>
+    <div className="overflow-y-scroll h-[84vh]">
+      <div id="bar-chart">
+        <ReactApexChart
+          options={barOptions}
+          series={barOptions.series}
+          type="bar"
+          height={barOptions.chart.height}
+        />
+      </div>
+      <div className="flex justify-between">
+        <div id="pie-chart">
+          <ReactApexChart
+            options={pieOptions}
+            series={pieSeries}
+            type="pie"
+            width={pieOptions.chart.width}
+          />
+        </div>
+        <div id="radial-chart-1">
+          <ReactApexChart
+            options={radialOptions}
+            series={radialOptions.series}
+            type="radialBar"
+            height={radialOptions.chart.height}
+          />
+        </div>
+        <div id="radial-chart-2">
+          <ReactApexChart
+            options={radialOptions}
+            series={radialOptions.series}
+            type="radialBar"
+            height={radialOptions.chart.height}
+          />
         </div>
       </div>
-      <div className="container bg-[#fff] flex items-center justify-center w-[40%] rounded-2xl py-5">
-        {" "}
-        <Space direction="vertical" align="center">
-          <QRCode value={text || "-"} />
-          <Input
-            placeholder="-"
-            maxLength={60}
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-        </Space>
-      </div>
-    </>
+    </div>
   );
-}
+};
+
+export default PieChart;
--- src/page/layout/index.tsx
+++ src/page/layout/index.tsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 import Header from "../../components/header";
 export default function Layout() {
   return (
-    <div className="flex bg-[#060B26]">
+    <div className="flex bg-[#060B26] owflow-hidden">
       <Sidebar />
       <div className="flex flex-col w-full">
         <Header />
-        <main className="">
+        <main>
           <Outlet></Outlet>
         </main>
       </div>
